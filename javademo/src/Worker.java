849d4a20466b92f8df9f5ed202f5885b19008442
FineDreams <1411374327@qq.com>
2017-12-06 19:20:56 +0800

    add Worker

---
+++ javademo/src/Worker.java
@@ -0,0 +1,25 @@
+public class Worker {
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public Worker(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
