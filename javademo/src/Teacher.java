c9bc9704a80adde417a299a3bbb8ff89584ef3c7
OnTheRightWay <34029301+OnTheRightWay@users.noreply.github.com>
2017-12-06 19:20:46 +0800

    add Teacher

---
+++ javademo/src/Teacher.java
@@ -0,0 +1,14 @@
+public class Teacher extends Person {
+    public Teacher(String id){
+        this.id=id;
+    }
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
